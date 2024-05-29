2dbf594835514a4b1d1b31e6c7d1c2a14a683740
Bret May <megabert3001@gmail.com>
2024-05-28 21:22:43 -0400

    Initial Commit

---
+++ Schedule Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Schedule Generator
+//
+//  Created by Bret May on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Schedule Generator/Schedule_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule_GeneratorApp.swift
+//  Schedule Generator
+//
+//  Created by Bret May on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Schedule_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
